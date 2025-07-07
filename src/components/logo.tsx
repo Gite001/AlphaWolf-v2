fb4b8e873dbe1c1e54fd7aaeea209fbc5b98f469
Yousso Yassou <bayoussef.lat@gmail.com>
2025-07-07 22:39:38 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -9,7 +9,7 @@ export const Logo = ({ className }: LogoProps) => {
     return (
         <div className={cn('relative', className)}>
             <Image
-                src="/images/logo.png"
+                src="https://placehold.co/128x128.png"
                 alt="AlphaWolf Logo"
                 fill
                 className="object-contain"
