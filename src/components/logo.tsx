4d97dfc4c252b4a44883aa2c4f202337142ad026
Yousso Yassou <bayoussef.lat@gmail.com>
2025-07-07 22:40:43 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -9,7 +9,7 @@ export const Logo = ({ className }: LogoProps) => {
     return (
         <div className={cn('relative', className)}>
             <Image
-                src="https://placehold.co/128x128.png"
+                src="/images/logo.png"
                 alt="AlphaWolf Logo"
                 fill
                 className="object-contain"
